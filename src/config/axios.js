71a6f112a22e549908ec8af4661756d8629b54eb
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-08-06 22:45:53 -0500

    Api url updated

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const axiosClient = () => {
   return axios.create({
-    baseURL: 'http://localhost:5000/api/',
+    //baseURL: 'http://localhost:5000/api/',
+    baseURL: '/api/',
     withCredentials: true
   });
 };
