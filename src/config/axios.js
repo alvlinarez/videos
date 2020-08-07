4dea5888429d65270be338acc6cae10979f19852
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-08-06 22:48:53 -0500

    Api url updated

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosClient = () => {
   return axios.create({
     //baseURL: 'http://localhost:5000/api/',
-    baseURL: '/api/',
+    baseURL: '/alg-videos-server/',
     withCredentials: true
   });
 };
