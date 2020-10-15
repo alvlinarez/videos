3a9efa1b9d34b582c1cc7cb84fa55a944984f029
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-10-15 16:58:56 -0500

    Axios url updated

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosClient = () => {
   return axios.create({
     //baseURL: 'http://localhost:5000/api/',
-    baseURL: '/videos-server/api/',
+    baseURL: 'https://videos.alvlinarez.dev/videos-server/api/',
     withCredentials: true
   });
 };
